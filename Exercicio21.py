6ff5e1aed310568a51cc34ae9708694ee2829bdd
Nicolas <nicolasdasilvasantos2009@gmail.com>
2026-05-12 19:18:41 -0300

    Resolvendo Exercício 21

---
+++ Exercicio21.py
@@ -0,0 +1,7 @@
+try:
+    n1=int(input("Digite um numero: "))
+    n2=int(input("Digite outro numero: "))
+    soma = n1 + n2
+    print("resultado da soma é: ",soma)
+except:
+    print("erro de entrada")
